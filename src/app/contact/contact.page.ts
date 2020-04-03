2b162af002d3a013f4cbf9f6e703f3a7542d417b
jérémy <j-mariette@protonmail.com>
2020-04-03 14:39:32 +0200

    projet ionic

---
+++ src/app/contact/contact.page.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit,NgModule} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import { AlertController,NavController} from '@ionic/angular';
+
+//import { Tab1Page } from '../tab1/tab1.page';
+import { Injectable } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.page.html',
+  styleUrls: ['./contact.page.scss'],
+})
+export class ContactPage implements OnInit {
+ 
+  name:string;
+  age:number;
+  nom:string;
+  response:any;
+
+  constructor( private route: ActivatedRoute) { 
+    
+  }
+   
+  ngOnInit() {
+    this.response = this.route.params.subscribe(params => {
+      this.nom = params['nom'];
+      this.age = params['age']; 
+      console.log("params "+this.nom+ " "+ this.age);
+ });
+   
+  }
+
+  detailParams(){
+    console.log("tu as cliqué "+this.name);
+    if(this.age > 17){
+      console.log("tu es majeur, tu as "+this.age);
+      
+    }else{
+      console.log("tu es mineur, tu as "+this.age);
+    }
+  }
+  
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);  // { first: '', last: '' }
+    console.log(form.valid);  // false
+  }
+
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertController,NavController} from '@ionic/angular';
+import { ContactPage } from '../contact/contact.page';
+import { stringify } from 'querystring';
+
+import { Push, PushObject, PushOptions } from '@ionic-native/push/ngx';
+import {NgForm} from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
+  nom:string;
+  age:number;
+  constructor(private navCtrl : NavController, private router: Router){
 
-  constructor() {}
+  }
+
+  ngOnInit(){
+    
+  }
+
+  onSubmit(form: NgForm){
+    
+   // console.log("envoi " +form.value.nom);
+    this.router.navigate(['contact',{nom: form.value.nom, age:form.value.age}]);
+
+    //this.router.navigateByUrl('/contact');
+  }
 
 }
